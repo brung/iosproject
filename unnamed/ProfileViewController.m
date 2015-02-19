815d7654f97734de51c8471242f3bc55fe8e2c71
daneozxy <daneozxy@yahoo-inc.com>
2015-02-19 09:21:47 -0800

    ProfileViewController initial look

--- unnamed/ProfileViewController.m
+++ unnamed/ProfileViewController.m
@@ -7,8 +7,32 @@
 //
 
 #import "ProfileViewController.h"
+#import "SurveyCell.h"
 
-@interface ProfileViewController ()
+NSString * const kSurveyCellName = @"SurveyCell";
+
+@interface ProfileViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSString* profileImageUrlString;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+//############################################################
+@property (weak, nonatomic) IBOutlet UILabel *profileNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *postCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answersCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answeredCountLabel;
+//############################################################
+//orderSegmentedControl decides the order questions of a user is shown in the
+//questionTableView
+//[0]Recent - based on time the question is created
+//[1]Open - still time order but only questions that hasn't been closed yet
+//[2]Popular - based on the number of participants of a question
+@property (weak, nonatomic) IBOutlet UISegmentedControl *orderSegmentedControl;
+//############################################################
+@property (weak, nonatomic) IBOutlet UITableView *questionTableView;
+@property (nonatomic, assign) CGFloat tableHeight;
+//############################################################
+@property (nonatomic, strong) UITableViewCell *prototypeCell;
+
 
 @end
 
@@ -19,6 +43,7 @@
     if (self) {
         self.title = @"Profile";
         self.tabBarItem.image = [UIImage imageNamed:@"User Male"];
+        self.tableHeight = 0;
     }
     return self;
 }
@@ -26,11 +51,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // - set profile image
+    [self.profileImageView setImage:[UIImage imageNamed:@"tongue" ]];
+    
+    // - tableview related:
+    self.questionTableView.dataSource = self;
+    self.questionTableView.delegate = self;
+    [self.questionTableView registerNib:[UINib nibWithNibName:kSurveyCellName bundle:nil] forCellReuseIdentifier:kSurveyCellName];
+    //self.questionTableView.rowHeight = UITableViewAutomaticDimension;
+    //NSLog(@"before reload, rowHeight set to %lf", self.questionTableView.rowHeight);
+    
+    // - refresh table content
+    [self.questionTableView reloadData];
+    
+    // - set table height just equal to content height so that
+    // - table view wont scroll, we use scrollview 's scroll only.
+    //CGRect tableFrame = [self.questionTableView frame];
+    //tableFrame.size.height = self.tableHeight;
+    //[self.questionTableView setFrame:tableFrame];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (!self.prototypeCell)
+    {
+        self.prototypeCell = [self.questionTableView dequeueReusableCellWithIdentifier:kSurveyCellName];
+    }
+    //configure cell content here
+    [self.prototypeCell layoutIfNeeded];
+    CGSize size = [self.prototypeCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    return size.height;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSInteger count = 5;
+    return count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    SurveyCell *cell = [self.questionTableView dequeueReusableCellWithIdentifier:kSurveyCellName];
+    //configure cell content here
+    
+    CGSize size = [cell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
+    NSLog(@"when create a cell, cell height is %lf", size.height);
+    self.tableHeight = self.tableHeight+size.height;
+    NSLog(@"so far tableHeight is %lf", self.tableHeight);
+
+    return cell;
 }
 
 /*
