8a458080aaac9f4c577480e259519e33d5127b22
Casing Chu <casingchu@yahoo-inc.com>
2015-02-19 11:23:19 -0800

    Add Initial Home View with SurveyHeaderViews and AnswerCell

---
+++ unnamed/AnswerCell.h
@@ -0,0 +1,15 @@
+//
+//  AnswerCell.h
+//  unnamed
+//
+//  Created by Casing Chu on 2/19/15.
+//  Copyright (c) 2015 com.yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *percentLabel;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+
+@end
---
+++ unnamed/SurveyHeaderView.h
@@ -0,0 +1,16 @@
+//
+//  SurveyHeaderView.h
+//  unnamed
+//
+//  Created by Casing Chu on 2/19/15.
+//  Copyright (c) 2015 com.yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Survey.h"
+
+@interface SurveyHeaderView : UITableViewHeaderFooterView
+
+@property (nonatomic, strong) Survey* survey;
+
+@end
