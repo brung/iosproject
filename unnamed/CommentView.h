c88b2ba93a6bc9a29245e67c11f60b621da29b00
Casing Chu <casing@gmail.com>
2015-02-23 23:21:00 -0800

    Add Comment View to SurveyViewCell

---
+++ unnamed/CommentView.h
@@ -0,0 +1,13 @@
+//
+//  CommentView.h
+//  unnamed
+//
+//  Created by Casing Chu on 2/23/15.
+//  Copyright (c) 2015 com.yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentView : UIView
+
+@end
