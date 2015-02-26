3a46b8765f033710616f1cf668bf3c012fba29cc
daneozxy <daneozxy@yahoo-inc.com>
2015-02-25 23:26:00 -0800

    unnamed.xcodeproj/project.pbxproj

---
+++ unnamed/CommentCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentCell.h
+//  unnamed
+//
+//  Created by Xiangyu Zhang on 2/25/15.
+//  Copyright (c) 2015 com.yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentCell : UITableViewCell
+
+@end
